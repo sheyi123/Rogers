960699ee20ba33f2ad3946b51abfa7206576384d
sheyi123 <144812426+sheyi123@users.noreply.github.com>
2023-09-12 06:45:18 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_5sd4gle", "template_l4g43fo", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
